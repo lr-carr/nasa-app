f08d61ad14f9178c5ea8a8ba3fe9ffeb93b1e1c3
Lydia Carr <lydia.carr@paconsulting.com>
2020-06-28 10:31:04 +0100

    Pass search data back into results page and add re-search form for Mars Rover search

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ def return_page_with_photo_info(photo_date, find_photo_text):
 	img_url = nasa_img_of_day_data['url']
 
 	date = photo_date.strftime("%a %-d %B, %Y")
-	print(date)
 
 	return render_template("photo.html", explanation=img_expl, title=img_title, image=img_url, date=date, find_photo_text=find_photo_text)
 
@@ -42,9 +41,11 @@ def past_photo_page():
 	date = datetime.strptime(form_date, "%Y-%m-%d").date()
 	return return_page_with_photo_info(date, "Find another photo of the day")
 
+
 @app.route("/mars")
 def mars_page():
 	return render_template("mars.html")
+
 
 @app.route("/mars-photo", methods=['POST'])
 def mars_photo_page():
@@ -52,27 +53,18 @@ def mars_photo_page():
 	sol = request.form["sol"]
 	rover = request.form["rover"]
 
-
-	print("camera type: " + camera_type)
-	print("sol: " + sol)
-	print("rover: " + rover)
-
 	url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos'
 	payload = {"api_key": config.api_key, "sol": sol, "camera": camera_type}
-	
 	response = requests.get(url, params=payload)
 	
 	mars_rover_data = response.json()
-
-	print(mars_rover_data)
-
 	photo_results = mars_rover_data["photos"]
 
 	if not photo_results:
 		return render_template("noresultsmars.html")
 	else:
-		image_url = photo_results[0]["img_src"]
-		return render_template("marsphoto.html", image=image_url)
+		first_image_url = photo_results[0]["img_src"]
+		return render_template("marsphoto.html", image=first_image_url, camera=camera_type, sol=sol, rover=rover.title())
 
 
 @app.route("/about")
