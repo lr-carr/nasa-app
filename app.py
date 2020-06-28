69ca66f3b6a3e80e03646614564ba4bc466bc40a
Lydia Carr <lydia.carr@paconsulting.com>
2020-06-28 10:17:33 +0100

    Added basic mars rover photo search functionality

--- app.py
+++ app.py
@@ -42,10 +42,37 @@ def past_photo_page():
 	date = datetime.strptime(form_date, "%Y-%m-%d").date()
 	return return_page_with_photo_info(date, "Find another photo of the day")
 
-
 @app.route("/mars")
 def mars_page():
 	return render_template("mars.html")
+
+@app.route("/mars-photo", methods=['POST'])
+def mars_photo_page():
+	camera_type = request.form["camera"]
+	sol = request.form["sol"]
+	rover = request.form["rover"]
+
+
+	print("camera type: " + camera_type)
+	print("sol: " + sol)
+	print("rover: " + rover)
+
+	url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos'
+	payload = {"api_key": config.api_key, "sol": sol, "camera": camera_type}
+	
+	response = requests.get(url, params=payload)
+	
+	mars_rover_data = response.json()
+
+	print(mars_rover_data)
+
+	photo_results = mars_rover_data["photos"]
+
+	if not photo_results:
+		return render_template("noresultsmars.html")
+	else:
+		image_url = photo_results[0]["img_src"]
+		return render_template("marsphoto.html", image=image_url)
 
 
 @app.route("/about")
